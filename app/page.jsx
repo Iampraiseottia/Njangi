545269abc45ef788d37699553565f4eba9ecd6d3
OTTIA PRAISE BETECK <ottiapraise50@gmail.com>
2025-02-16 02:19:31 +0100

    project setuped

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '@styles/globals.css'
+
+const Home = () => {
+  return (
+    <div className=' text-red-900 pt-10 '>Home</div>
+  )
+}
+
+export default Home     
