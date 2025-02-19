b3bc8f89348483b8d589683cd8528266b9f80580
OTTIA PRAISE BETECK <ottiapraise50@gmail.com>
2025-02-19 18:31:48 +0100

    about-you right done

--- app/about-you/page.jsx
+++ app/about-you/page.jsx
@@ -24,16 +24,16 @@ return (
     <br />
     
     <FontAwesomeIcon icon={faUser} className='werey '  />  
-
+ 
   </div>
 
   {/* Registration Page 01 Right Section */}
   <div className='w-full lg:w-[55%] p-6 lg:p-9'>
-    <h1 className='text-3xl lg:text-5xl font-extrabold tracking-wider mt-6  text-center mb-5'>SIGN UP</h1>
+    <h1 className='text-3xl lg:text-5xl font-extrabold tracking-wider mt-6  text-center mb-5'>Start Your Journey 🔥</h1>
     <br />
     <form className='flex flex-col gap-6 w-full max-w-xl'>
       <div className='flex flex-col gap-2'>
-        <label htmlFor="country" className='font-semibold text-lg tracking-wide'>Country:</label>
+        <label htmlFor="country" className='font-semibold text-lg tracking-wide'>🏴󠁥󠁳󠁰󠁶󠁿 Country:</label>
         <input 
           type="text" 
           name="country" 
@@ -44,7 +44,7 @@ return (
       </div>
 
       <div className='flex flex-col gap-2'>
-        <label htmlFor="homeAddress" className='font-semibold text-lg tracking-wide'>State or Region:</label>
+        <label htmlFor="homeAddress" className='font-semibold text-lg tracking-wide'>🌆 State or Region:</label>
         <input 
           type="text" 
           name="homeAddress" 
@@ -55,38 +55,38 @@ return (
       </div>
 
       <div className='flex flex-col gap-2'>
-        <label htmlFor="email" className='font-semibold text-lg tracking-wide'>Email Address:</label>
+        <label htmlFor="email" className='font-semibold text-lg tracking-wide'>🏙️ City:</label>
         <input 
-          type="email" 
-          name="email" 
-          id="email" 
-          placeholder='Your Email Address' 
+          type="text" 
+          name="city" 
+          id="city" 
+          placeholder='Your City / Town' 
           className='w-full text-base bg-transparent rounded-xl border-2 border-[#0ef] py-3 px-4 focus:ring-1 focus:ring-[#0ef] focus:outline-none duration-300 placeholder-white'
         />
       </div>
 
       <div className='flex flex-col gap-2'>
-        <label htmlFor="number" className='font-semibold text-lg tracking-wide'>Phone Number:</label>
+        <label htmlFor="number" className='font-semibold text-lg tracking-wide'>🏠 Home Address:</label>
         <input 
-          type="number" 
-          name="number" 
-          id="number" 
-          placeholder='Your Phone Number' 
+          type="text" 
+          name="homeAddress" 
+          id="homeAddress" 
+          placeholder='Your Home Address / Precise Location' 
           className='w-full text-base bg-transparent rounded-xl border-2 border-[#0ef] py-3 px-4 focus:ring-1 focus:ring-[#0ef] focus:outline-none duration-300 placeholder-white'
         />
       </div>
 
-      <button className='mt-3 bg-gradient-to-r from-[#0ef] via-slate-700 to-[#0ef] w-full text-white py-4 px-6 font-extrabold text-xl lg:text-2xl duration-500 rounded-sm hover:rounded-[40px] hover:opacity-95 cursor-pointer flex justify-center items-center'>
+      <Link href="/income" className='mt-3 bg-gradient-to-r from-[#0ef] via-slate-700 to-[#0ef] w-full text-white py-4 px-6 font-extrabold text-xl lg:text-2xl duration-500 rounded-sm hover:rounded-[40px] hover:opacity-95 cursor-pointer flex justify-center items-center'>
         Continue ➡️
-      </button>
+      </Link>
     </form>
 
     <p className='text-right mt-4 mb-2 font-bold text-lg'>02/05</p>
     
-    <button className='mt-3 bg-gradient-to-r from-[#0ef] via-slate-700 to-[#0ef] w-[90%] text-white py-4 px-6 font-extrabold text-xl lg:text-2xl duration-500 rounded-sm hover:rounded-[40px] hover:opacity-95 cursor-pointer flex justify-center items-center'>
+    <Link href="/register" className='mt-3 bg-gradient-to-r from-[#0ef] via-slate-700 to-[#0ef] w-[90%] text-white py-4 px-6 font-extrabold text-xl lg:text-2xl duration-500 rounded-sm hover:rounded-[40px] hover:opacity-95 cursor-pointer flex justify-center items-center'>
         Go Back 
-      </button>
-    
+      </Link>
+     
   </div>
 </section>
 </main>
