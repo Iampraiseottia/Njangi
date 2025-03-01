be26ad9b774f21b1f78f06ffa7de57b617ece676
OTTIA PRAISE BETECK <ottiapraise50@gmail.com>
2025-03-01 22:45:47 +0100

    footer structured

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className=''>
+      <div className=''>Footer Sec 1</div>
+      <div className=''>Footer Sec 2</div>
+      <div className=''>Footer Sec 3</div>
+      <div className=''>Footer Sec 4</div>
+    </section>
   )
 }
 
