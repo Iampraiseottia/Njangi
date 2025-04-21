87c0505b47401fcdd27c0cd2a91c018eadeb5c2d
OTTIA PRAISE BETECK <ottiapraise50@gmail.com>
2025-04-21 13:57:30 +0100

    Settings removed and side bar remodeled

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,26 +1,28 @@
+import React, { Suspense } from "react";
 
-import React, { Suspense } from 'react';
+import Sidebar from "../components/Sidebar";
 
-import DashBoardContent from '../components/dashboardContent'
-
-import globalStyle from '../globals.css' 
-
+import globalStyle from "../globals.css";
 
 const DashBoard = () => {
   return (
-    <Suspense fallback={<div className="flex items-center justify-center h-screen">
-      <div id="fountainG">
-        <div id="fountainG_1" class="fountainG"></div>
-        <div id="fountainG_2" class="fountainG"></div>
-        <div id="fountainG_3" class="fountainG"></div>
-        <div id="fountainG_4" class="fountainG"></div>
-        <div id="fountainG_5" class="fountainG"></div>
-        <div id="fountainG_6" class="fountainG"></div>
-        <div id="fountainG_7" class="fountainG"></div>
-        <div id="fountainG_8" class="fountainG"></div>
-      </div>
-    </div>}>
-      <DashBoardContent />
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center h-screen">
+          <div id="fountainG">
+            <div id="fountainG_1" class="fountainG"></div>
+            <div id="fountainG_2" class="fountainG"></div>
+            <div id="fountainG_3" class="fountainG"></div>
+            <div id="fountainG_4" class="fountainG"></div>
+            <div id="fountainG_5" class="fountainG"></div>
+            <div id="fountainG_6" class="fountainG"></div>
+            <div id="fountainG_7" class="fountainG"></div>
+            <div id="fountainG_8" class="fountainG"></div>
+          </div>
+        </div>
+      }
+    >
+      <Sidebar />
     </Suspense>
   );
 };
