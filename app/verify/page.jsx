827e51e55879e3230f7cb4d3d2583bb7d118d998
OTTIA PRAISE BETECK <ottiapraise50@gmail.com>
2025-09-10 17:34:23 +0100

    Verification page for codes sents UI updated to fit both dark and light themes

--- app/verify/page.jsx
+++ app/verify/page.jsx
@@ -12,6 +12,9 @@ import Metadata from "../components/Metadata";
 
 import { motion } from "motion/react";
 
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
 const Verify = () => {
   const router = useRouter();
   const [error, setError] = useState("");
@@ -75,113 +78,124 @@ const Verify = () => {
   };
 
   return (
-    <main className="flex justify-center items-center w-full min-h-screen bg-gray-800 text-white p-4">
+    <main>
       <Metadata title={metadata.title} description={metadata.description} />
 
-      <motion.section
-        initial={{ opacity: 0, y: 100 }}
-        whileInView={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, delay: 0.5 }}
-        className="sm:w-full md:w-[80%] lg:w-[40%] max-w-7xl bg-transparent border-2 border-blue-600  overflow-hidden rounded-lg wrapper4 my-8"
-      >
-        <div className="w-full lg:w-[100%] p-6 lg:p-9">
-          <motion.h1
-            initial={{ opacity: 0, y: 100 }}
-            whileInView={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.7, delay: 0.7 }}
-            className="text-3xl lg:text-5xl font-extrabold tracking-wider mt-6 text-center mb-5 flex justify-center items-center"
-          >
-            <img
-              src="/logo3.png"
-              alt="Njangi Logo"
-              className="w-20 h-20 mr-1"
-            />
-            NJANGIFY
-          </motion.h1>
-          <br />
+      {/* Navbar */}
+      <Navbar />
 
-          <form
-            onSubmit={handleSubmit}
-            className="flex flex-col gap-6 w-full justify-center align-middle items-center"
-          >
-            <div className="ease-in-out transition-all w-full">
-              <motion.label
-                initial={{ opacity: 0, y: 100 }}
-                whileInView={{ y: 0, opacity: 1 }}
-                transition={{ duration: 0.7, delay: 0.7 }}
-                htmlFor="verification_code"
-                className="text-left text-[22px] font-medium tracking-wider"
-              >
-                Verification Code
-              </motion.label>
-
-              {error && (
-                <motion.p
-                  initial={{ opacity: 0, y: 100 }}
-                  whileInView={{ y: 0, opacity: 1 }}
-                  transition={{ duration: 0.1, delay: 0.2 }}
-                  className="text-red-500 my-3 text-center font-medium"
-                >
-                  {error}
-                </motion.p>
-              )}
-
-              <motion.div
-                initial={{ opacity: 0, y: 100 }}
-                whileInView={{ y: 0, opacity: 1 }}
-                transition={{ duration: 0.7, delay: 0.7 }}
-                className="mt-6 mb-1 flex flex-row justify-center items-center"
-              >
-                {[0, 1, 2, 3].map((index) => (
-                  <input
-                    key={index}
-                    type="text"
-                    ref={codeRefs[index]}
-                    value={codeValues[index]}
-                    onChange={(e) => handleCodeChange(index, e)}
-                    onKeyDown={(e) => handleKeyDown(index, e)}
-                    onMouseEnter={() => onMouseEnterCode(index)}
-                    inputMode="numeric"
-                    pattern="[0-9]*"
-                    maxLength={1}
-                    placeholder={index + 1}
-                    className={`w-[25%] text-center text-base bg-transparent rounded-xl border-2 ${
-                      codeErrors[index] ? "border-red-500" : "border-blue-600 "
-                    } py-3 px-4 focus:ring-1 focus:ring-blue-600  focus:outline-none duration-300 placeholder-white ${
-                      index < 3 ? "mr-3" : ""
-                    }`}
-                  />
-                ))}
-              </motion.div>
-            </div>
-
-            <motion.button
+      <div className="flex justify-center items-center w-full min-h-[70vh] dark:bg-gray-700 dark:text-white bg-gray-100 text-gray-900 p-4">
+        <motion.section
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          className="sm:w-full md:w-[80%] lg:w-[40%] max-w-7xl bg-transparent border-[1px] border-blue-600  overflow-hidden rounded-lg wrapper4 my-8 bg-slate-300 dark:bg-slate-600 "
+        >
+          <div className="w-full lg:w-[100%] p-6 lg:p-9">
+            <motion.h1
               initial={{ opacity: 0, y: 100 }}
               whileInView={{ y: 0, opacity: 1 }}
               transition={{ duration: 0.7, delay: 0.7 }}
-              type="submit"
-              className="mt-3 bg-gradient-to-r from-blue-600  via-slate-700 to-blue-600  w-full hover:from-blue-500  hover:via-slate-600 hover:to-blue-500  text-white py-4 px-6 font-extrabold text-xl lg:text-2xl duration-500 rounded-sm hover:rounded-[40px] hover:opacity-95 cursor-pointer flex justify-center items-center tracking-wider"
+              className="text-3xl lg:text-5xl font-extrabold tracking-wider mt-6 text-center mb-5 flex justify-center items-center"
             >
-              <FontAwesomeIcon icon={faLock} className="mr-1 w-6 h-6" /> VERIFY
-            </motion.button>
-          </form>
+              <img
+                src="/logo3.png"
+                alt="Njangi Logo"
+                className="w-20 h-20 mr-1"
+              />
+              NJANGIFY
+            </motion.h1>
+            <br />
 
-          <motion.p
-            initial={{ opacity: 0, y: 100 }}
-            whileInView={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.7, delay: 0.7 }}
-            className="text-center mt-4 text-white tracking-wide"
-          >
-            Go Back to{" "}
-            <Link
-              href="/login"
-              className="text-blue-600  ml-1 font-extrabold hover:cursor-pointer hover:underline duration-300"
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col gap-6 w-full justify-center align-middle items-center"
             >
-              LOGIN
-            </Link>
-          </motion.p>
-        </div>
-      </motion.section>
+              <div className="ease-in-out transition-all w-full">
+                <motion.label
+                  initial={{ opacity: 0, y: 100 }}
+                  whileInView={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.7, delay: 0.7 }}
+                  htmlFor="verification_code"
+                  className="text-left text-[22px] font-medium tracking-wider"
+                >
+                  Verification Code
+                </motion.label>
+
+                {error && (
+                  <motion.p
+                    initial={{ opacity: 0, y: 100 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 0.1, delay: 0.2 }}
+                    className="text-red-500 my-3 text-center font-medium"
+                  >
+                    {error}
+                  </motion.p>
+                )}
+
+                <motion.div
+                  initial={{ opacity: 0, y: 100 }}
+                  whileInView={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.7, delay: 0.7 }}
+                  className="mt-6 mb-1 flex flex-row justify-center items-center"
+                >
+                  {[0, 1, 2, 3].map((index) => (
+                    <input
+                      key={index}
+                      type="text"
+                      ref={codeRefs[index]}
+                      value={codeValues[index]}
+                      onChange={(e) => handleCodeChange(index, e)}
+                      onKeyDown={(e) => handleKeyDown(index, e)}
+                      onMouseEnter={() => onMouseEnterCode(index)}
+                      inputMode="numeric"
+                      pattern="[0-9]*"
+                      maxLength={1}
+                      placeholder={index + 1}
+                      className={`w-[25%] text-center text-base bg-transparent rounded-xl border-2  ${
+                        codeErrors[index]
+                          ? "border-red-500"
+                          : "border-blue-600 "
+                      } py-3 px-4 focus:ring-1 focus:ring-blue-600  focus:outline-none duration-300 placeholder-white dark:placeholder-slate-400 ${
+                        index < 3 ? "mr-3" : ""
+                      }`}
+                    />
+                  ))}
+                </motion.div>
+              </div>
+
+              <motion.button
+                initial={{ opacity: 0, y: 100 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.7, delay: 0.7 }}
+                type="submit"
+                className="mt-3 bg-gradient-to-r from-blue-600  via-slate-700 to-blue-600  w-full hover:from-blue-500  hover:via-slate-600 hover:to-blue-500  text-white py-4 px-6 font-extrabold text-xl lg:text-2xl duration-500 rounded-sm hover:rounded-[40px] hover:opacity-95 cursor-pointer flex justify-center items-center tracking-wider"
+              >
+                <FontAwesomeIcon icon={faLock} className="mr-1 w-6 h-6" />{" "}
+                VERIFY
+              </motion.button>
+            </form>
+
+            <motion.p
+              initial={{ opacity: 0, y: 100 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.7, delay: 0.7 }}
+              className="text-center mt-4 text-white tracking-wide"
+            >
+              Go Back to{" "}
+              <Link
+                href="/login"
+                className="text-blue-600  ml-1 font-extrabold hover:cursor-pointer hover:underline duration-300"
+              >
+                LOGIN
+              </Link>
+            </motion.p>
+          </div>
+        </motion.section>
+      </div>
+
+      {/* Footer  */}
+      <Footer />
     </main>
   );
 };
